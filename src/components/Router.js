b11d09e1c73575758203e4707e098bbee1176226
baedollee <baesee0401@gmail.com>
2022-12-20 19:21:00 +0900

    12/20

---
+++ src/components/Router.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Routes } from 'react-router-dom';
+
+const Router = () => {
+  const [isLogIn, setIsLogIn] = useState();
+
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
+};
+
+export default Router;
